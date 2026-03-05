32ad5b5be6f13b3f41abb2037734d9df7589f06c
jeff <6269vy@gmail.com>
2026-03-05 15:32:21 +0800

    Resolve merge conflict: keep prefetch_factor=1

--- train_hipas_monai_unet3d_5fold.py
+++ train_hipas_monai_unet3d_5fold.py
@@ -335,7 +335,6 @@ def run_one_fold(fold_idx: int):
         prefetch_factor=2,
 =======
         prefetch_factor=1,
->>>>>>> master
         collate_fn=list_data_collate,
     )
     val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=0, pin_memory=False)

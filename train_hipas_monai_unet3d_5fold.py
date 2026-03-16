f2a177024efc74a456361d2b47aca2eb782a6505
jeff <6269vy@gmail.com>
2026-03-16 20:56:44 +0800

    fix validation killed

--- train_hipas_monai_unet3d_5fold.py
+++ train_hipas_monai_unet3d_5fold.py
@@ -47,7 +47,7 @@ PATCH_SAMPLES_PER_CASE = 10       # 每個 case 抽幾個 patch（RandCropByLabe
 #sum(patch) = 160*PATCH_SAMPLES_PER_CASE
 
 # validation sliding window
-VAL_ROI_SIZE = (64,128,128)
+VAL_ROI_SIZE = (48,96,96)
 VAL_OVERLAP = 0.25
 
 # 训练超参
@@ -64,7 +64,7 @@ NUM_WORKERS = 1
 
 # cache（RAM 夠可調高）
 CACHE_RATE_TRAIN = 0.2
-CACHE_RATE_VAL = 0.1
+CACHE_RATE_VAL = 0.0
 
 # 5-fold
 N_FOLDS = 5
@@ -317,7 +317,7 @@ def run_one_fold(fold_idx: int):
 
     logger.info(f"device={device}")
     wandb.init(
-        project="hipas_unet3d_5fold",
+        project="hipas_unet3d_5fold_1600patch",
         name=f"fold_{fold_idx}",
         group="5fold_cv",
         config={
@@ -345,7 +345,7 @@ def run_one_fold(fold_idx: int):
         shuffle=True,
         num_workers=NUM_WORKERS,
         pin_memory=False,
-        persistent_workers=True,
+        persistent_workers=False,
         prefetch_factor=1,
         collate_fn=list_data_collate,
     )
